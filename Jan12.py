9c6863d5f70b14ca418558782c3e4d83dc211ba9
shres-ruby <59715671+shres-ruby@users.noreply.github.com>
2020-01-12 18:41:14 +0545

    Add files via upload

---
+++ Jan12.py
@@ -0,0 +1,23 @@
+# users = ['Ram', 'Sita', 'Gita']
+
+# for user in users:
+#     print (user)
+
+# data = [[2,4,5],
+#         ['a','b','c','d'],
+#         [12,45,78]]
+
+# for x in data:
+#     for a in x:
+#         print (a)
+
+x = 0
+list = []
+while x<5:
+    name = [input('Enter employee name : ')]
+    list.append(name)
+    x+=1
+
+for i in list:
+    print (i)
+
